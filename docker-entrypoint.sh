76af557d0c073f2aa807ec24f8de5cb9109be104
Pavel Prischepa <pprischepa@gmail.com>
2016-07-13 17:33:25 +0600

    Updated Drupal 7 Nginx config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,12 +2,12 @@
 
 set -eo pipefail
 
-if [ -z "$NGINX_APP" ]; then
-    NGINX_APP="drupal8"
+if [ -z "$APP_ENV_DRUPAL_VERSION" ]; then
+    APP_ENV_DRUPAL_VERSION=8
 fi
 
 if [ ! "$(ls -A /etc/nginx/conf.d)" ]; then
-    cp /opt/${NGINX_APP}.conf /etc/nginx/conf.d/
+    cp /opt/drupal${APP_ENV_DRUPAL_VERSION}.conf /etc/nginx/conf.d/
 fi
 
 exec nginx -g "daemon off;"
