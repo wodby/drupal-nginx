37e012db6f09aebbb3e857d58d321348763aada2
Pavel Prischepa <pprischepa@gmail.com>
2017-01-13 11:37:04 +0700

    Upgraded to 1.10.2

--- 1.10/docker-entrypoint.sh
+++ 1.10/docker-entrypoint.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-set -eo pipefail
+set -e
+
+if [[ ! -z $DEBUG ]]; then
+  set -x
+fi
 
 # Set upstream name
 sed -i 's/UPSTREAM_NAME/'"${NGINX_UPSTREAM_NAME}"'/' /etc/nginx/nginx.conf
@@ -11,17 +15,17 @@ if [ -z "$DRUPAL_VERSION" ]; then
 fi
 
 # Copy default nginx config.
-if [ ! "$(ls -A /etc/nginx/conf.d)" ]; then
+if [[ ! "$(ls -A /etc/nginx/conf.d)" ]]; then
     cp /opt/drupal${DRUPAL_VERSION}.conf /etc/nginx/conf.d/
 fi
 
 # Configure docroot.
-if [ -n "$NGINX_DOCROOT" ]; then
+if [[ -n "$NGINX_DOCROOT" ]]; then
     sed -i 's@root /var/www/html/;@'"root /var/www/html/${NGINX_DOCROOT};"'@' /etc/nginx/conf.d/*.conf
 fi
 
 # Ensure server name defined.
-if [ -z "$NGINX_SERVER_NAME" ]; then
+if [[ -z "$NGINX_SERVER_NAME" ]]; then
     NGINX_SERVER_NAME=localhost
 fi
 
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# drupal-nginx
+[![Build Status](https://travis-ci.org/wodby/node.svg?branch=master)](https://travis-ci.org/wodby/node)
+[![Docker Pulls](https://img.shields.io/docker/pulls/wodby/node.svg)](https://hub.docker.com/r/wodby/node)
+[![Docker Stars](https://img.shields.io/docker/stars/wodby/node.svg)](https://hub.docker.com/r/wodby/node)
+
+# Supported tags and respective `Dockerfile` links
+
+-       [`1.10`, `latest` (*1.10/Dockerfile*)](https://github.com/wodby/node/tree/master/1.10/Dockerfile)
