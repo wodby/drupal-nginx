631a159ed303dbfa5771e7e78178c6d2ea38ad05
Chingis Sandanov <chingis@wodby.com>
2017-10-03 18:50:44 +0700

    Remove unnecessary permissions fix

--- 6/1.x/init/drupal-nginx.sh
+++ 6/1.x/init/drupal-nginx.sh
@@ -6,7 +6,5 @@ if [[ -n "${DEBUG}" ]]; then
     set -x
 fi
 
-sudo fix-permissions.sh www-data www-data "${WODBY_DIR_FILES}" "${HTML_DIR}"
-
 gotpl /etc/gotpl/drupal.conf.tpl > /etc/nginx/conf.d/drupal.conf
 gotpl /etc/gotpl/fastcgi_params.tpl > /etc/nginx/fastcgi_params
