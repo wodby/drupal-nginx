f09b0b412a591e3cff8af6812e747a0b2264c43f
Chingis Sandanov <chingis@wodby.com>
2017-10-03 20:31:27 +0700

    Set volumes ownership to www-data

--- 6/1.x/init/drupal-nginx.sh
+++ 6/1.x/init/drupal-nginx.sh
@@ -6,5 +6,7 @@ if [[ -n "${DEBUG}" ]]; then
     set -x
 fi
 
+sudo fix-permissions.sh www-data www-data "${HTML_DIR}" "${WODBY_DIR_FILES}"
+
 gotpl /etc/gotpl/drupal.conf.tpl > /etc/nginx/conf.d/drupal.conf
 gotpl /etc/gotpl/fastcgi_params.tpl > /etc/nginx/fastcgi_params
